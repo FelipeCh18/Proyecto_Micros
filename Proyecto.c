5ca4520791931c36d16e02fb9fb7df3c0e9f37fc
FelipeCh18 <fchaves@unal.edu.co>
2023-05-28 19:21:45 -0500

    Creacion del proyecto

---
+++ Proyecto.c
@@ -0,0 +1,11 @@
+#include<xc.h> //Libreria para PIC
+#include<stdio.h> //Libreria adicional para manejo de decimales
+#include "LibLCDXC8.h" //Libreria para uso de LCD
+
+#pragma config FOSC=INTOSC_EC //Sentencia para usar oscilador externo
+#pragma config WDT=OFF //Apagar el perro guardian
+#pragma config PBADEN=OFF 
+#pragma config LVP=OFF
+
+void main(void) {
+}
